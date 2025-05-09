a1ed44c0f8a4b0ebd722eb3b706c23826cc4bc13
Bruno Sousa <bruno.sousa@profiz.com.br>
2025-05-09 16:04:13 -0300

    first commit app

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+ singleQuote: true,
+ arrowParens: 'avoid',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+};
