a39114afc9fd8abb92b128ce6cb1874eea9e679f
Bruno Sousa <bruno.sousa@profiz.com.br>
2025-05-09 16:35:17 -0300

    build: configure docker and postgres db

--- src/plugins/config.ts
+++ src/plugins/config.ts
@@ -1,13 +1,15 @@
-import "dotenv/config";
-import fp from "fastify-plugin";
-import { FastifyPluginAsync } from "fastify";
-import { Static, Type } from "@sinclair/typebox";
-import Ajv from "ajv";
+import 'dotenv/config';
+import fp from 'fastify-plugin';
+import { FastifyPluginAsync } from 'fastify';
+import { Static, Type } from '@sinclair/typebox';
+import Ajv from 'ajv';
+import fastifyPlugin from 'fastify-plugin';
+import fastifyPostgres from '@fastify/postgres';
 
 export enum NodeEnv {
-  development = "development",
-  test = "test",
-  production = "production",
+  development = 'development',
+  test = 'test',
+  production = 'production',
 }
 
 const ConfigSchema = Type.Object({
@@ -27,22 +29,27 @@ const ajv = new Ajv({
 
 export type Config = Static<typeof ConfigSchema>;
 
-const configPlugin: FastifyPluginAsync = async (server) => {
+const configPlugin: FastifyPluginAsync = async server => {
   const validate = ajv.compile(ConfigSchema);
   const valid = validate(process.env);
   if (!valid) {
-    throw new Error(
-      ".env file validation failed - " +
-        JSON.stringify(validate.errors, null, 2)
-    );
+    throw new Error('.env file validation failed - ' + JSON.stringify(validate.errors, null, 2));
   }
-  server.decorate("config", process.env as Config);
+  server.decorate('config', process.env as Config);
 };
 
-declare module "fastify" {
+declare module 'fastify' {
   interface FastifyInstance {
     config: Config;
   }
 }
 
+const postgresPlugin = fastifyPlugin(async fastify => {
+  fastify.register(fastifyPostgres, {
+    connectionString:
+      process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/expense_tracker',
+  });
+});
+
 export default fp(configPlugin);
+export { postgresPlugin };
