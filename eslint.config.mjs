edb15081f199b72b84c2829757bb064689d1b33f
Bruno Sousa <bruno.sousa@profiz.com.br>
2025-05-09 16:21:49 -0300

    chore: setup config eslint v9 + prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,36 +1,60 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from 'eslint-define-config';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [{
-    ignores: ["coverage/*", "node_modules/*", "build/*", ".prettierrc.js"],
-}, ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-), {
+export default defineConfig([
+  { ignores: ['coverage/*', 'node_modules/*', 'build/*', '.prettierrc.js'] },
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ),
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
+      '@typescript-eslint': typescriptEslint,
+      prettier: require('eslint-plugin-prettier'),
     },
-
     languageOptions: {
-        parser: tsParser,
+      parser: tsParser,
     },
-
     rules: {
-        "@typescript-eslint/explicit-module-boundary-types": 0,
-        "@typescript-eslint/camelcase": 0,
-        "@typescript-eslint/no-var-requires": 0,
+      '@typescript-eslint/explicit-module-boundary-types': 0,
+      '@typescript-eslint/camelcase': 0,
+      '@typescript-eslint/no-var-requires': 0,
+      'prettier/prettier': 'error',
     },
-}];
+  },
+  {
+    env: {
+      node: true,
+      es2021: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'prettier'],
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+]);
